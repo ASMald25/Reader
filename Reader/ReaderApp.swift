6bc9666ae48b02ade3bc379aba59ab01907677e9
ASMald25 <108498427+ASMald25@users.noreply.github.com>
2025-08-12 20:58:19 -0700

    Initial Commit

---
+++ Reader/ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderApp.swift
+//  Reader
+//
+//  Created by Alexander Maldonado on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
