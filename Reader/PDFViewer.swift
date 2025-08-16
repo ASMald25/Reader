4d6e544ff5ab6165e5190cd21f65d3c8baa31cf6
ASMald25 <108498427+ASMald25@users.noreply.github.com>
2025-08-15 20:34:54 -0700

    made enum to make VisualSettings global such as color

--- Reader/PDFViewer.swift
+++ Reader/PDFViewer.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 import PDFKit
 
+enum VisualSettings {
+    static let customColor = NSColor(red: 156/255.0,
+                                     green: 101/255.0,
+                                     blue: 46/255.0,
+                                     alpha: 0.50)
+}
+
 struct PDFKitView: NSViewRepresentable {
     let url: URL
-    
     func makeNSView(context: Context) -> PDFView {
        let pdfView = PDFView()
+        //Reader window settings
         pdfView.autoScales = true
+        pdfView.backgroundColor = VisualSettings.customColor
+        
         if let document = PDFDocument(url: url) {
             pdfView.document = document
         }
