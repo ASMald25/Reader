989d7d8340dd349487f7640b08e1cdcf58974b62
ASMald25 <108498427+ASMald25@users.noreply.github.com>
2025-08-17 15:19:42 -0700

    Made extension of NSColor to turn hexcoded color into NSColor object. Changed enum of custom colors to fit extension

--- Reader/PDFViewer.swift
+++ Reader/PDFViewer.swift
@@ -7,13 +7,32 @@
 
 import SwiftUI
 import PDFKit
+import AppKit
+
+
+//turn hex into a NSColor object
+extension NSColor {
+    convenience init?(hex: String, alpha: CGFloat = 1.0) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+
+        let r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let b = CGFloat(rgb & 0x0000FF) / 255.0
+
+        self.init(red: r, green: g, blue: b, alpha: alpha)
+    }
+}
 
 enum VisualSettings {
-    static let customColor = NSColor(red: 156/255.0,
-                                     green: 101/255.0,
-                                     blue: 46/255.0,
-                                     alpha: 0.50)
+    static let customBackColor = NSColor(hex: "#c85637") ?? NSColor.black
+    static let complementaryColor = NSColor(hex: "#37A9C8") ?? NSColor.white
 }
+
+
 
 struct PDFKitView: NSViewRepresentable {
     let url: URL
@@ -21,7 +40,7 @@ struct PDFKitView: NSViewRepresentable {
        let pdfView = PDFView()
         //Reader window settings
         pdfView.autoScales = true
-        pdfView.backgroundColor = VisualSettings.customColor
+        pdfView.backgroundColor = VisualSettings.customBackColor
         
         if let document = PDFDocument(url: url) {
             pdfView.document = document
