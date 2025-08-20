9bcb2445fe0bf6fd9a34866cf489f8f7b986c75d
ASMald25 <108498427+ASMald25@users.noreply.github.com>
2025-08-20 14:18:47 -0700

    Added encodebookmark to encode dict of string:Bookmark struct, added decodeBookmark that decodes bookmarks.json or returns empty dict if it fails

--- Reader/JSONHandling.swift
+++ Reader/JSONHandling.swift
@@ -17,8 +17,44 @@ struct Bookmark: Codable{
 
 //dictionary to both check if pdf has been bookmarked before or to create new bookmarks in a pdf
 //inout to avoid modifying copies of dict, pass reference to it
-func addOrUpdateBookmark(_ dict: inout [String: Bookmark], filename: URL, bookmarkPgNum: Int){
+func addOrUpdateBookmark(_ dict: inout [String: Bookmark], filename: URL, bookmarkPgNum: Int) -> String{
     var pdfName: String = filename.lastPathComponent
     dict[pdfName] = Bookmark(fileName: pdfName, bookmarkPgNum:  bookmarkPgNum)
     
+    return pdfName
+    
+}
+
+//receive dict of Bookmark structs, encode every struct in dict
+func encodeBookmark(_ dict: inout[String: Bookmark], fileName: String = "bookmarks.json" ) {
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
+    do {
+        let data = try encoder.encode(dict)
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        
+        try data.write(to: fileURL)
+        print("Book marks saved to",fileURL.path)
+        
+    }catch{
+        print("Failed to encode Bookmarks:", error)
+    }
+}
+
+func decodeBookmark(from fileName: String = "bookmarks.json") -> [String: Bookmark] {
+    let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    let fileURL = documentsDirectory.appendingPathComponent(fileName)
+    
+    do {
+        let data = try Data(contentsOf: fileURL)
+        
+        let decoder = JSONDecoder()
+        let bookmarksDict = try decoder.decode([String: Bookmark].self, from: data)
+        
+        return bookmarksDict
+    } catch {
+        print("failed to load or decode json bookmarks", error)
+        return [:]
+    }
 }
