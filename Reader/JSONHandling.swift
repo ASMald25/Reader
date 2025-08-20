df3471f4773678b72afe1621fd2ad180bbb903f9
ASMald25 <108498427+ASMald25@users.noreply.github.com>
2025-08-20 13:31:33 -0700

    Added JSON handling to manage to bookmarks. Implementing functionality to both store bookmark information in a struct, and manage those structs in a dictionary.

--- Reader/JSONHandling.swift
+++ Reader/JSONHandling.swift
@@ -5,3 +5,20 @@
 //  Created by Alexander Maldonado on 8/20/25.
 //
 
+import Foundation
+
+//manages bookmarks based on the name of the file
+struct Bookmark: Codable{
+    let fileName: String
+    let bookmarkPgNum: Int
+    
+}
+
+
+//dictionary to both check if pdf has been bookmarked before or to create new bookmarks in a pdf
+//inout to avoid modifying copies of dict, pass reference to it
+func addOrUpdateBookmark(_ dict: inout [String: Bookmark], filename: URL, bookmarkPgNum: Int){
+    var pdfName: String = filename.lastPathComponent
+    dict[pdfName] = Bookmark(fileName: pdfName, bookmarkPgNum:  bookmarkPgNum)
+    
+}
